246c96d3ef93a7f5e7dc1fb780ffae837edde51e
posh <mrposhboo@gmail.com>
2024-04-19 00:30:22 +0100

    function

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python3
-"""
-Module to define a function add that takes 
-two float arguments and returns their sum.
-"""
+
+""" Module to define a function """
+
 
 def add(a: float, b: float) -> float:
-    """
-    Function to add two floats.
+    """Function returns a True Statement"""
 
-    Args:
-        a (float): The first number.
-        b (float): The second number.
-
-    Returns:
-        float: The sum of a and b.
-    """
     return a + b
