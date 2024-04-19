d6fce23d91851bf08a3d268c131855aeac0f31c5
posh <mrposhboo@gmail.com>
2024-04-19 01:45:20 +0100

    corrections

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module to define a function.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Function to add two floats.
+
+    """
+    return a + b
+
+if __name__ == "__main__":
+    add(1.11, 2.22)
