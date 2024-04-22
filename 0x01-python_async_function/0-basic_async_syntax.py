e4d54b774dea63e95c98b5877cd1f921385e5c5e
posh <mrposhboo@gmail.com>
2024-04-22 23:30:51 +0100

    new

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module on an asynchronous coroutine
+"""
+
 import asyncio
 import random
-from typing import List
 
-async def wait_random(max_delay: int = 10) -> List[float]:
+async def wait_random(max_delay=10):
     await asyncio.sleep(random.uniform(0, max_delay))
-    return [max_delay]
+    return max_delay
 
 # Test cases
-async def main() -> None:
+async def main():
     print(await wait_random())
     print(await wait_random(5))
     print(await wait_random(15))
