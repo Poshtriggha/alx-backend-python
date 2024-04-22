a1bd40b4949b95d92a6094ad362a9e5134b91628
posh <mrposhboo@gmail.com>
2024-04-22 23:15:40 +0100

    jkk

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+import asyncio
+import random
+
+async def wait_random(max_delay=10):
+    await asyncio.sleep(random.uniform(0, max_delay))
+    return max_delay
+
+# Test cases
+async def main():
+    print(await wait_random())
+    print(await wait_random(5))
+    print(await wait_random(15))
+
+if __name__ == "__main__":
+    asyncio.run(main())
