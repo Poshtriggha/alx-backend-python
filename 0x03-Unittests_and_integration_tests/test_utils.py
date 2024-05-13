cafa72e4f732f2f1bbb9e2e6d29254da03e1de99
posh <mrposhboo@gmail.com>
2024-05-13 19:50:12 +0100

    NestedMap

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
-"""Unit tests for utility functions"""
+"""AAAA"""
 
-from unittest import TestCase
+
+from unittest import TestCase, mock
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 from utils import access_nested_map, get_json
 
-class TestAccessNestedMap(TestCase):
-    """Test cases for access_nested_map function"""
 
+class TestAccessNestedMap(TestCase):
+    """AAAA"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected_outcome):
-        """Test access_nested_map function"""
+        """AAAA"""
         actual_outcome = access_nested_map(nested_map, path)
         self.assertEqual(actual_outcome, expected_outcome)
 
@@ -24,29 +25,28 @@ class TestAccessNestedMap(TestCase):
         ({"a": 1}, ("a", "b"), 'b')
     ])
     def test_access_nested_map_exception(self, nested_map, path, expctd_msg):
-        """Test access_nested_map function for KeyError"""
+        """AAAA"""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
             self.assertEqual(expctd_msg, context.exception)
 
 
 class TestGetJson(TestCase):
-    """Test cases for get_json function"""
+    """AAAA"""
 
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
     def test_get_json(self, test_url, test_payload):
-        """Test get_json function"""
+        """AAAA"""
 
-        # Mock the return value of requests.get
+        #  return value of requests.get
         mock_response = Mock()
         mock_response.json.return_value = test_payload
-        
         with patch('requests.get', return_value=mock_response):
-            # Call the get_json function
+    
             actual_response = get_json(test_url)
-            # Assert that the actual_response is equal to test_payload
+        
             self.assertEqual(actual_response, test_payload)
             mock_response.json.assert_called_once()
